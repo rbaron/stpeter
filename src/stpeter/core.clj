1cb9cc20e0c8c3608820d4977aa2d81dab6c9e11
Raphael Baron <rbaron@rbaron.net>
2017-06-22 11:46:51 -0300

    Proper commands validation

--- src/stpeter/core.clj
+++ src/stpeter/core.clj
@@ -10,21 +10,38 @@
 
 (def to-esp (async/chan (async/sliding-buffer 1024)))
 
+(def help
+  (str "Available commands:" \newline
+       "set temp [17-26]" \newline
+       "set ac off"))
+
+(defn handle-cmd
+  [cmd out-chan]
+  (if-let [off-cmd (re-find #"^set ac off$" cmd)]
+    (do (async/go (async/>! to-esp cmd))
+        (async/go (async/>! out-chan "Ok!")))
+    (if-let [[_ temp] (re-find #"^set temp (\d+)$" cmd)]
+      (let [int-temp (Integer. temp)]
+        (if (and (>= int-temp 17) (<= int-temp 26))
+          (do (async/go (async/>! to-esp cmd))
+              (async/go (async/>! out-chan "Ok!")))
+          (async/go (async/>! out-chan "Invalid temp. Keep it >= 17 and <= 26"))))
+      (async/go (async/>! out-chan (str "Invalid command. " help))))))
+
 (defn handle-msg
   [msg out-chan my-user-id]
-  (println "pattern" my-user-id (get msg :text))
-  (if (re-matches (re-pattern (str ".*" my-user-id ".*")) (get msg :text ""))
-    (async/go
-      (async/>! to-esp (:text msg))
-      (async/>! out-chan {:type "message"
-                          :channel (:channel msg)
-                          :text "Ok found my username!"}))))
+  (let [text (get msg :text "")
+        pattern (re-pattern (str "<@" my-user-id "> (.*)"))
+        matcher (re-matcher pattern text)]
+      (if-let [[_ cmd] (re-find matcher)]
+        (handle-cmd cmd out-chan))))
 
 (defn wait-and-send-to-esp
   [s info]
   (async/go-loop []
     (if-let [msg (async/<! to-esp)]
-      (let [put-res @(s/put! s (str (json/write-str msg) \newline))]
+      ;(let [put-res @(s/put! s (str (json/write-str msg) \newline))]
+      (let [put-res @(s/put! s (str msg \newline))]
         (if put-res
           (recur)
           (println "Cannot put message to esp (conn probably closed by client)")))
--- test/stpeter/core_test.clj
+++ test/stpeter/core_test.clj
@@ -1,7 +1,38 @@
 (ns stpeter.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.core.async :as async]
+            [clojure.test :refer :all]
             [stpeter.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest handle-msg-test
+  (testing "It handles valid temp messages"
+    (let [out-chan (async/chan)
+          my-user-id "my-user-id"]
+      (handle-msg {:text "<@my-user-id> set temp 17"} out-chan my-user-id)
+      (let [to-esp-msg (async/<!! to-esp)
+            to-slack-msg (async/<!! out-chan)]
+        (is (= to-esp-msg "set temp 17"))
+        (is (= to-slack-msg "Ok!")))))
+
+  (testing "It handles turn ac off message"
+    (let [out-chan (async/chan)
+          my-user-id "my-user-id"]
+      (handle-msg {:text "<@my-user-id> set ac off"} out-chan my-user-id)
+      (let [to-esp-msg (async/<!! to-esp)
+            to-slack-msg (async/<!! out-chan)]
+        (is (= to-esp-msg "set ac off"))
+        (is (= to-slack-msg "Ok!")))))
+
+  (testing "It handles invalid temp messages"
+    (let [out-chan (async/chan)
+          my-user-id "my-user-id"]
+      (handle-msg {:text "<@my-user-id> set temp 0"} out-chan my-user-id)
+      (let [to-slack-msg (async/<!! out-chan)]
+        (is (= to-slack-msg "Invalid temp. Keep it >= 17 and <= 26")))))
+
+  (testing "It handles invalid commands"
+    (let [out-chan (async/chan)
+          my-user-id "my-user-id"]
+      (handle-msg {:text "<@my-user-id> help"} out-chan my-user-id)
+      (let [to-slack-msg (async/<!! out-chan)]
+        (is (= to-slack-msg "Invalid command. Available commands:\nset temp [17-26]\nset ac off")))))
+)
