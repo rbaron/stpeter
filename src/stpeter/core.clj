1432ad95b003900abbdf91a495c86db52bcd1db2
Raphael Baron <rbaron@rbaron.net>
2017-06-22 11:55:56 -0300

    Proper messages to slack

--- src/stpeter/core.clj
+++ src/stpeter/core.clj
@@ -15,18 +15,24 @@
        "set temp [17-26]" \newline
        "set ac off"))
 
+(defn make-msg
+  [msg channel]
+  {:type "message"
+   :text msg
+   :channel channel})
+
 (defn handle-cmd
-  [cmd out-chan]
+  [cmd out-chan channel]
   (if-let [off-cmd (re-find #"^set ac off$" cmd)]
     (do (async/go (async/>! to-esp cmd))
-        (async/go (async/>! out-chan "Ok!")))
+        (async/go (async/>! out-chan (make-msg "Ok!" channel))))
     (if-let [[_ temp] (re-find #"^set temp (\d+)$" cmd)]
       (let [int-temp (Integer. temp)]
         (if (and (>= int-temp 17) (<= int-temp 26))
           (do (async/go (async/>! to-esp cmd))
-              (async/go (async/>! out-chan "Ok!")))
-          (async/go (async/>! out-chan "Invalid temp. Keep it >= 17 and <= 26"))))
-      (async/go (async/>! out-chan (str "Invalid command. " help))))))
+              (async/go (async/>! out-chan (make-msg "Ok!" channel))))
+          (async/go (async/>! out-chan (make-msg "Invalid temp. Keep it >= 17 and <= 26" channel)))))
+      (async/go (async/>! out-chan (make-msg (str "Invalid command. " help) channel))))))
 
 (defn handle-msg
   [msg out-chan my-user-id]
@@ -34,13 +40,12 @@
         pattern (re-pattern (str "<@" my-user-id "> (.*)"))
         matcher (re-matcher pattern text)]
       (if-let [[_ cmd] (re-find matcher)]
-        (handle-cmd cmd out-chan))))
+        (handle-cmd cmd out-chan (:channel msg)))))
 
 (defn wait-and-send-to-esp
   [s info]
   (async/go-loop []
     (if-let [msg (async/<! to-esp)]
-      ;(let [put-res @(s/put! s (str (json/write-str msg) \newline))]
       (let [put-res @(s/put! s (str msg \newline))]
         (if put-res
           (recur)
--- test/stpeter/core_test.clj
+++ test/stpeter/core_test.clj
@@ -7,32 +7,32 @@
   (testing "It handles valid temp messages"
     (let [out-chan (async/chan)
           my-user-id "my-user-id"]
-      (handle-msg {:text "<@my-user-id> set temp 17"} out-chan my-user-id)
+      (handle-msg {:text "<@my-user-id> set temp 17" :channel "c"} out-chan my-user-id)
       (let [to-esp-msg (async/<!! to-esp)
             to-slack-msg (async/<!! out-chan)]
         (is (= to-esp-msg "set temp 17"))
-        (is (= to-slack-msg "Ok!")))))
+        (is (= to-slack-msg (make-msg "Ok!" "c"))))))
 
   (testing "It handles turn ac off message"
     (let [out-chan (async/chan)
           my-user-id "my-user-id"]
-      (handle-msg {:text "<@my-user-id> set ac off"} out-chan my-user-id)
+      (handle-msg {:text "<@my-user-id> set ac off" :channel "c"} out-chan my-user-id)
       (let [to-esp-msg (async/<!! to-esp)
             to-slack-msg (async/<!! out-chan)]
         (is (= to-esp-msg "set ac off"))
-        (is (= to-slack-msg "Ok!")))))
+        (is (= to-slack-msg (make-msg "Ok!" "c"))))))
 
   (testing "It handles invalid temp messages"
     (let [out-chan (async/chan)
           my-user-id "my-user-id"]
-      (handle-msg {:text "<@my-user-id> set temp 0"} out-chan my-user-id)
+      (handle-msg {:text "<@my-user-id> set temp 0" :channel "c"} out-chan my-user-id)
       (let [to-slack-msg (async/<!! out-chan)]
-        (is (= to-slack-msg "Invalid temp. Keep it >= 17 and <= 26")))))
+        (is (= to-slack-msg (make-msg "Invalid temp. Keep it >= 17 and <= 26" "c"))))))
 
   (testing "It handles invalid commands"
     (let [out-chan (async/chan)
           my-user-id "my-user-id"]
-      (handle-msg {:text "<@my-user-id> help"} out-chan my-user-id)
+      (handle-msg {:text "<@my-user-id> help" :channel "c"} out-chan my-user-id)
       (let [to-slack-msg (async/<!! out-chan)]
-        (is (= to-slack-msg "Invalid command. Available commands:\nset temp [17-26]\nset ac off")))))
+        (is (= to-slack-msg (make-msg "Invalid command. Available commands:\nset temp [17-26]\nset ac off" "c"))))))
 )
